71e9cd6b9ed72ac23253f83d94156551614cac83
Charles Korn <me@charleskorn.com>
2022-04-21 21:51:27 +1000

    Don't report coverage for test helper files.

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@jest/types";
 const config: Config.InitialOptions = {
   clearMocks: true,
   collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.ts", "!**/node_modules/**"],
+  collectCoverageFrom: ["src/**/*.ts", "!**/node_modules/**", "!src/test-helpers/**"],
   errorOnDeprecated: true,
   transform: {
     "^.+\\.ts$": "ts-jest",
