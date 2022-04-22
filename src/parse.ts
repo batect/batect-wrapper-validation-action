c43fb1d734e2bad229acdc06d43b509b7a0999c5
Charles Korn <me@charleskorn.com>
2022-04-22 19:15:03 +1000

    Fix issue where linting wasn't applied to files in the `src` directory.

--- src/parse.ts
+++ src/parse.ts
@@ -1,3 +1,19 @@
+/*
+   Copyright 2017-2022 Charles Korn.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       httsp://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 export interface WrapperChecksums {
   unix: string;
   windows: string;
@@ -8,7 +24,7 @@ interface FileChecksum {
   checksum: string;
 }
 
-const checksumLinePattern = /^([0-9a-f]{64})  (.*)$/;
+const checksumLinePattern = /^([0-9a-f]{64}) {2}(.*)$/;
 
 const parseChecksumLine = (line: string): FileChecksum => {
   const match = line.match(checksumLinePattern);
@@ -18,8 +34,8 @@ const parseChecksumLine = (line: string): FileChecksum => {
   }
 
   return {
-    fileName: match![2],
-    checksum: match![1],
+    fileName: match[2],
+    checksum: match[1],
   };
 };
 
