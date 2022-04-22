3ba046c7950ee460f018153b05ef392b39e62ec7
Charles Korn <me@charleskorn.com>
2022-04-22 17:58:07 +1000

    Add license header to all files.

--- license.js
+++ license.js
@@ -13,6 +13,3 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-
-export { StaticFilesServer } from "./staticFilesServer";
-export { TestStatusReporter } from "./testStatusReporter";
